abe0f4c7409b32693b77a7ea5a2aba735cd737e4
Leandro Martins <leandro.martins.85@gmail.com>
2020-05-23 17:13:27 -0300

    Conexão com o banco

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import express from "express";
+import express from 'express';
+
+import './database';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello world!" });
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello world!' });
 });
 
 app.listen(3000, () => {
-  console.log("🚀 Server started on port 3000!");
+  console.log('🚀 Server started on port 3000!');
 });
